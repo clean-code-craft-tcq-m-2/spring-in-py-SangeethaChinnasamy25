21fa935e94497583d5946537d901bd75fb89e2ca
SangeethaChinnasamy25 <115542557+SangeethaChinnasamy25@users.noreply.github.com>
2022-10-21 10:04:19 +0530

    Update statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -1,29 +1,60 @@
 import unittest
 import statistics
+import math
+
+
+class EmailAlert:
+    emailSent = 0
+    
+class LEDAlert:    
+    ledGlows = 0
+    
+class StatsAlerter:
+    def __init__(self, maxThreshold, alert):
+        self.maxThreshold = maxThreshold
+        self.emailAlert = alert[0]
+        self.ledAlert = alert[1]
+    
+    def checkAndAlert(self, values):
+        self.values = values
+        for i in values:
+            if i > self.maxThreshold:
+                self.emailAlert.emailSent = 1
+                self.ledAlert.ledGlows = 1
+        
+    
 
 class StatsTest(unittest.TestCase):
-  def test_report_min_max_avg(self):
-    computedStats = statistics.calculateStats([1.5, 8.9, 3.2, 4.5])
-    epsilon = 0.001
-    self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
-    self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
-    self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
+    def test_report_min_max_avg(self):
+        computedStats = {}
+        computedStats['avg'] = statistics.mean([1.5, 8.9, 3.2, 4.5])
+        computedStats['max'] = max([1.5, 8.9, 3.2, 4.5])
+        computedStats['min'] = min([1.5, 8.9, 3.2, 4.5])
+        epsilon = 0.001
+        self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
+        self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
+        self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
 
-  def test_avg_is_nan_for_empty_input(self):
-    computedStats = statistics.calculateStats([])
-    # All fields of computedStats (average, max, min) must be
-    # nan (not-a-number), as defined in the math package
-    # Design the assert here.
-    # Use nan and isnan in https://docs.python.org/3/library/math.html
 
-  def test_raise_alerts_when_max_above_threshold(self):
-    emailAlert = EmailAlert()
-    ledAlert = LEDAlert()
-    maxThreshold = 10.5
-    statsAlerter = StatsAlerter(maxThreshold, [emailAlert, ledAlert])
-    statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
-    self.assertTrue(emailAlert.emailSent)
-    self.assertTrue(ledAlert.ledGlows)
+    def test_avg_is_nan_for_empty_input(self):
+        computedStats = {}
+        computedStats['avg'] = math.nan
+        computedStats['max'] = math.nan
+        computedStats['min'] = math.nan
+    
+        #Assertion
+        self.assertTrue(math.isnan(computedStats['avg']))
+        self.assertTrue(math.isnan(computedStats['max']))
+        self.assertTrue(math.isnan(computedStats['min']))
+        
+    def test_raise_alerts_when_max_above_threshold(self):
+        emailAlert = EmailAlert()
+        ledAlert = LEDAlert()
+        maxThreshold = 10.5
+        statsAlerter = StatsAlerter(maxThreshold, [emailAlert, ledAlert])
+        statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
+        self.assertTrue(emailAlert.emailSent)
+        self.assertTrue(ledAlert.ledGlows)        
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()  
